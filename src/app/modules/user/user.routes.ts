3b1c2bdaedafc77d0758350e921ed866f02818f0
Yeamin Foysal <yeaminfoysal14@gmail.com>
2025-11-01 23:16:25 +0600

    image file moved

--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -24,7 +24,7 @@ router.post(
 
 router.post(
     "/create-admin",
-    // auth(UserRole.ADMIN),
+    auth(UserRole.ADMIN),
     fileUploader.upload.single('file'),
     (req: Request, res: Response, next: NextFunction) => {
         req.body = UserValidation.createAdminValidationSchema.parse(JSON.parse(req.body.data))
