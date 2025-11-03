fcf503d0f3313ce00d26980401f655d2cb3756ce
Yeamin Foysal <yeaminfoysal10@gmail.com>
2025-11-03 09:37:31 +0600

    setup from office

--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -16,20 +16,22 @@ router.get(
 router.post(
     "/create-patient",
     fileUploader.upload.single('file'),
-    (req: Request, res: Response, next: NextFunction) => {
-        req.body = UserValidation.createPatientValidationSchema.parse(JSON.parse(req.body.data))
-        return userController.createPatient(req, res, next)
-    }
+    userController.createPatient
+    // (req: Request, res: Response, next: NextFunction) => {
+    //     req.body = UserValidation.createPatientValidationSchema.parse(JSON.parse(req.body.data))
+    //     return userController.createPatient(req, res, next)
+    //}
 )
 
 router.post(
     "/create-admin",
     auth(UserRole.ADMIN),
     fileUploader.upload.single('file'),
-    (req: Request, res: Response, next: NextFunction) => {
-        req.body = UserValidation.createAdminValidationSchema.parse(JSON.parse(req.body.data))
-        return userController.createAdmin(req, res, next)
-    }
+    userController.createAdmin
+    // (req: Request, res: Response, next: NextFunction) => {
+    //     req.body = UserValidation.createAdminValidationSchema.parse(JSON.parse(req.body.data))
+    //     return userController.createAdmin(req, res, next)
+    // }
 );
 
 router.post(
