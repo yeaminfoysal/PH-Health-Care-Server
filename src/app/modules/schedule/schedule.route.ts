502bad7a045b0d70b3347f7d1c9aafe13553ef9f
Yeamin Foysal <yeaminfoysal10@gmail.com>
2025-11-03 16:55:44 +0600

    schedude create by admin complete

---
+++ src/app/modules/schedule/schedule.route.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import auth from "../../middlewares/auth";
+import { UserRole } from "@prisma/client";
+import { ScheduleController } from "./schedule.controller";
+
+const router = express.Router();
+
+// router.get(
+//     "/",
+//     auth(UserRole.DOCTOR, UserRole.DOCTOR),
+//     ScheduleController.schedulesForDoctor
+// )
+
+router.post(
+    "/",
+    ScheduleController.insertIntoDB
+)
+
+
+// router.delete(
+//     "/:id",
+//     ScheduleController.deleteScheduleFromDB
+// )
+export const ScheduleRoutes = router;
