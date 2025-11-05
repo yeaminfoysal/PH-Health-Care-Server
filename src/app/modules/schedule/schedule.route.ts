10664ecbc8ea1c8a6dbceb94f50078c7e70bc74e
Yeamin Foysal <yeaminfoysal10@gmail.com>
2025-11-05 12:53:12 +0600

    doctor schedule creating complete

--- src/app/modules/schedule/schedule.route.ts
+++ src/app/modules/schedule/schedule.route.ts
@@ -7,7 +7,7 @@ const router = express.Router();
 
 router.get(
     "/",
-    // auth(UserRole.DOCTOR, UserRole.DOCTOR),
+    auth(UserRole.DOCTOR, UserRole.DOCTOR),
     ScheduleController.schedulesForDoctor
 )
 
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { userRoutes } from '../modules/user/user.routes';
 import { authRoutes } from '../modules/auth/auth.route';
 import { ScheduleRoutes } from '../modules/schedule/schedule.route';
+import { doctorScheduleRoutes } from '../modules/doctorSchedule/doctorSchedule.routes';
 
 const router = express.Router();
 
@@ -18,6 +19,10 @@ const moduleRoutes = [
         path: '/schedule',
         route: ScheduleRoutes
     },
+    {
+        path: '/doctor-schedule',
+        route: doctorScheduleRoutes
+    }
 ];
 
 moduleRoutes.forEach(route => router.use(route.path, route.route))
