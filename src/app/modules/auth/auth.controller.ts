ed64cddb70a1a079edfd2c06f8756e4951c8c297
Yeamin Foysal <yeaminfoysal10@gmail.com>
2025-11-17 13:03:44 +0600

    reset password, forgot password, change password, refresh token, get me complete

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import catchAsync from "../../shared/catchAsync";
 import sendResponse from "../../shared/sendResponse";
 import { AuthService } from "./auth.service";
+import httpStatus from "http-status";
 
 const login = catchAsync(async (req: Request, res: Response) => {
     const result = await AuthService.login(req.body);
@@ -30,6 +31,83 @@ const login = catchAsync(async (req: Request, res: Response) => {
     })
 })
 
+const refreshToken = catchAsync(async (req: Request, res: Response) => {
+    const { refreshToken } = req.cookies;
+
+    const result = await AuthService.refreshToken(refreshToken);
+    res.cookie("accessToken", result.accessToken, {
+        secure: true,
+        httpOnly: true,
+        sameSite: "none",
+        maxAge: 1000 * 60 * 60,
+    });
+
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: "Access token genereated successfully!",
+        data: {
+            message: "Access token genereated successfully!",
+        },
+    });
+});
+
+const changePassword = catchAsync(
+    async (req: Request & { user?: any }, res: Response) => {
+        const user = req.user;
+
+        const result = await AuthService.changePassword(user, req.body);
+
+        sendResponse(res, {
+            statusCode: httpStatus.OK,
+            success: true,
+            message: "Password Changed successfully",
+            data: result,
+        });
+    }
+);
+
+const forgotPassword = catchAsync(async (req: Request, res: Response) => {
+    await AuthService.forgotPassword(req.body);
+
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: "Check your email!",
+        data: null,
+    });
+});
+
+const resetPassword = catchAsync(async (req: Request, res: Response) => {
+    const token = req.headers.authorization || "";
+
+    await AuthService.resetPassword(token, req.body);
+
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: "Password Reset!",
+        data: null,
+    });
+});
+
+const getMe = catchAsync(async (req: Request, res: Response) => {
+    const userSession = req.cookies;
+    const result = await AuthService.getMe(userSession);
+
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: "User retrive successfully!",
+        data: result,
+    });
+});
+
 export const AuthController = {
-    login
+    login,
+    refreshToken,
+    changePassword,
+    resetPassword,
+    forgotPassword,
+    getMe
 }
--- src/app/modules/auth/auth.route.ts
+++ src/app/modules/auth/auth.route.ts
@@ -1,11 +1,45 @@
 import express from 'express'
 import { AuthController } from './auth.controller';
+import auth from '../../middlewares/auth';
+import { UserRole } from '@prisma/client';
+
 
 const router = express.Router();
+
+router.get(
+    "/me",
+    AuthController.getMe
+)
 
 router.post(
     "/login",
     AuthController.login
 )
+
+router.post(
+    '/refresh-token',
+    AuthController.refreshToken
+)
+
+router.post(
+    '/change-password',
+    auth(
+        UserRole.ADMIN,
+        UserRole.DOCTOR,
+        UserRole.PATIENT
+    ),
+    AuthController.changePassword
+);
+
+router.post(
+    '/forgot-password',
+    AuthController.forgotPassword
+);
+
+router.post(
+    '/reset-password',
+    AuthController.resetPassword
+)
+
 
 export const authRoutes = router;
